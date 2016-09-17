ec16839375e37199259092453f7443e9ab495e19
Calvin <Calvin.lacc@gmail.com>
2016-09-17 13:38:34 -0600

    Basic views for the mailer is set up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default :from => "from@example.com"
+
+  def welcome(user)
+    @user = user
+    mail(to: @user.email, subject: 'Welcome BOI!')
+  end
+
 end
